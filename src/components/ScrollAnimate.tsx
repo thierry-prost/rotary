54f9aa31fd780605caeb7cf603cca80e0d36386c
Thierry Prost <thierry.prost@quantech.eu>
2022-05-12 19:51:29 +0200

    split scroll animate and title

---
+++ src/components/ScrollAnimate.tsx
@@ -0,0 +1,78 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import Box from "@mui/material/Box";
+import Fade from "@mui/material/Fade";
+import Slide, { SlideProps } from "@mui/material/Slide";
+import React, { FC, useRef, useState } from "react";
+import ReactVisibilitySensor from "react-visibility-sensor";
+
+interface ScrollAnimateProps {
+  delayMs?: number;
+  durationMs?: number;
+  heightPx?: number;
+  children: React.ReactElement;
+  direction?: SlideProps["direction"];
+}
+
+export const ScrollAnimate: FC<ScrollAnimateProps> = ({
+  children,
+  delayMs: delay = 0,
+  durationMs: dur = 600,
+  heightPx = 30,
+  direction = "up",
+}) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [visible, setVisible] = useState<boolean>(false);
+  return (
+    <Box>
+      <Container direction={direction} ref={containerRef} heightPx={heightPx} />
+      <ReactVisibilitySensor
+        partialVisibility
+        onChange={(v: boolean) => setVisible(v)}
+      >
+        <Fade
+          in={visible}
+          timeout={{ enter: 2 * dur + delay, exit: 0 }}
+          style={{ transitionDelay: `${visible ? delay : 0}ms` }}
+        >
+          <div>
+            <HideDelay delay={delay} visible={visible}>
+              <Slide
+                in={visible}
+                direction={direction}
+                timeout={dur + delay}
+                style={{ transitionDelay: `${visible ? delay : 0}ms` }}
+                container={containerRef.current}
+              >
+                {children}
+              </Slide>
+            </HideDelay>
+          </div>
+        </Fade>
+      </ReactVisibilitySensor>
+    </Box>
+  );
+};
+
+const Container = styled.div<{
+  heightPx: number;
+  direction: SlideProps["direction"];
+}>`
+  position: absolute;
+  height: ${(props) => props.heightPx}px;
+  width: ${(props) => props.heightPx}px;
+  ${(props) => props.direction === "right" && "right: 0;"}
+  ${(props) => props.direction === "left" && "left: 0;"}
+`;
+
+const hide = keyframes`
+  from, to {
+    visibility: visible;
+  }
+`;
+
+const HideDelay = styled.div<{ delay: number; visible: boolean }>`
+  visibility: hidden;
+  animation: ${hide} 0ms ${({ visible, delay }) => (visible ? delay : 0)}ms
+    forwards;
+`;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,9 @@
 import { FunctionComponent } from "react";
 import { ReactComponent as Education } from "../assets/Education.svg";
 import { Tile } from "../components/Tile";
-import { ThemeProvider, useTheme } from "@emotion/react";
-import { createTheme, Grid, Typography, useMediaQuery } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { Grid, Typography, useMediaQuery } from "@mui/material";
+import { ScrollAnimate } from "../components/ScrollAnimate";
 
 interface HomeProps {}
 
@@ -13,58 +14,62 @@ const Home: FunctionComponent<HomeProps> = () => {
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} md={6} lg={4}>
-        <Tile title="We are so great" icon={Education}>
-          <div>hi</div>
-        </Tile>
+        <ScrollAnimate>
+          <Tile title="We are so great" icon={Education}>
+            <div>hi</div>
+          </Tile>
+        </ScrollAnimate>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <Tile
-          title="We are so great"
-          delay={md || lg ? 300 : 0}
-          icon={Education}
-        >
-          <Typography variant="body2">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat.
-          </Typography>
-        </Tile>
+        <ScrollAnimate delayMs={md || lg ? 300 : 0}>
+          <Tile title="We are so great" icon={Education}>
+            <Typography variant="body2">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat.
+            </Typography>
+          </Tile>
+        </ScrollAnimate>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <Tile title="We are so great" delay={lg ? 600 : 0} icon={Education}>
-          <Typography variant="body2">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat.
-          </Typography>
-        </Tile>
+        <ScrollAnimate delayMs={lg ? 600 : 0}>
+          <Tile title="We are so great" icon={Education}>
+            <Typography variant="body2">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat.
+            </Typography>
+          </Tile>
+        </ScrollAnimate>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <Tile title="We are so great" icon={Education} delay={md ? 300 : 0}>
-          <div>hi</div>
-        </Tile>
+        <ScrollAnimate delayMs={md ? 300 : 0}>
+          <Tile title="We are so great" icon={Education}>
+            <div>hi</div>
+          </Tile>
+        </ScrollAnimate>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <Tile title="We are so great" delay={lg ? 300 : 0} icon={Education}>
-          <Typography variant="body2">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat.
-          </Typography>
-        </Tile>
+        <ScrollAnimate delayMs={md ? 300 : 0}>
+          <Tile title="We are so great" icon={Education}>
+            <Typography variant="body2">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat.
+            </Typography>
+          </Tile>
+        </ScrollAnimate>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <Tile
-          title="We are so great"
-          delay={lg ? 600 : md ? 300 : 0}
-          icon={Education}
-        >
-          <Typography variant="body2">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat.
-          </Typography>
-        </Tile>
+        <ScrollAnimate delayMs={lg ? 600 : md ? 300 : 0}>
+          <Tile title="We are so great" icon={Education}>
+            <Typography variant="body2">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat.
+            </Typography>
+          </Tile>
+        </ScrollAnimate>
       </Grid>
     </Grid>
   );
